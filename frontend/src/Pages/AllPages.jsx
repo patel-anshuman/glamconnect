f0c59dbc25f1cfff2e132a2ff25035de858acedc
Anshuman Patel <patelanshuman6@gmail.com>
2023-07-19 21:55:35 +0530

    frontend signup & login page

--- frontend/src/Pages/AllPages.jsx
+++ frontend/src/Pages/AllPages.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes ,Route} from 'react-router-dom'
 import Home from "./Home"
 import Services from "./Services"
+import Signup from "./Signup"
+import Login from './Login'
+
 const AllPages = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/services' element={<Services/>}/>
-        <Route path='/services' element={<Services/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
         <Route path='*' element={<h1>Page Not Found</h1>}/>
     </Routes>
   )

175e60ffdd9b18ade63d87014de254d30d8427eb
Anshuman Patel <patelanshuman6@gmail.com>
2023-07-20 22:37:22 +0530

    homepage & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import AllPages from './Pages/AllPages';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 function App() {
   return (
     <>
       <Navbar />
       <AllPages />
+      <Footer />
     </>
   );
 }
